5f0d2c2c6a14d8e5410be17590f4be6965a2593b
cahayadeeputrii <desyanaputricahaya@gmail.com>
2025-05-21 09:41:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("haaiii")
 st.write(
-    "belajar bersama pacar jaehyun"
+    "belajar bersama pacar  jaehyun"
 ) 
 st.image("633854d94bd9271f3dcda1161bcec58e.jpg", width=200) 
